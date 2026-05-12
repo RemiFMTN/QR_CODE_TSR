199190d850b982df635f24b4e00d41caca2c8656
Rémi Formentin <remi.formentin@gmail.com>
2026-05-12 16:02:09 +0200

    Modification de la page d'acceuil

--- backend/src/index.js
+++ backend/src/index.js
@@ -17,6 +17,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/health", (req, res) => res.json({ ok: true }));
+app.get("/", (req, res) => res.redirect("/public/registration.html"));
 
 app.use("/auth", authRoutes);
 app.use("/groups", authMiddleware, groupRoutes);
